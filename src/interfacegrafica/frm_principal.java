3040581ecdff7e4ffbf5021ee29ce909abbc76ab
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-09-22 21:15:40 -0300

    botoes novos

--- src/interfacegrafica/frm_principal.java
+++ src/interfacegrafica/frm_principal.java
@@ -5,6 +5,8 @@
  */
 package interfacegrafica;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author henrique.medeiros
@@ -27,6 +29,7 @@ public class frm_principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -35,6 +38,10 @@ public class frm_principal extends javax.swing.JFrame {
         txtResultado = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        rbSomar = new javax.swing.JRadioButton();
+        rbSubtrair = new javax.swing.JRadioButton();
+        rbMultiplicar = new javax.swing.JRadioButton();
+        rbDividir = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("SENAC");
@@ -54,6 +61,8 @@ public class frm_principal extends javax.swing.JFrame {
             }
         });
 
+        txtResultado.setEditable(false);
+        txtResultado.setBackground(new java.awt.Color(255, 255, 255));
         txtResultado.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtResultadoActionPerformed(evt);
@@ -69,35 +78,74 @@ public class frm_principal extends javax.swing.JFrame {
             }
         });
 
+        buttonGroup1.add(rbSomar);
+        rbSomar.setText("Somar");
+        rbSomar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbSomarActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(rbSubtrair);
+        rbSubtrair.setText("Subtrair");
+        rbSubtrair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbSubtrairActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(rbMultiplicar);
+        rbMultiplicar.setText("Multiplicar");
+
+        buttonGroup1.add(rbDividir);
+        rbDividir.setText("Dividir");
+        rbDividir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbDividirActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(172, 172, 172)
+                        .addComponent(jButton1)
+                        .addGap(0, 58, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(42, 42, 42)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel3)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel4))
+                            .addComponent(jLabel2))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(txtNum2, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
-                            .addComponent(txtNum1)
-                            .addComponent(txtResultado)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(158, 158, 158)
-                        .addComponent(jButton1)))
-                .addContainerGap(97, Short.MAX_VALUE))
+                            .addComponent(txtNum1)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(42, 42, 42)
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtResultado)))
+                .addGap(60, 60, 60))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(rbMultiplicar)
+                    .addComponent(rbSubtrair, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(rbSomar, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(rbDividir, javax.swing.GroupLayout.Alignment.LEADING))
+                .addGap(139, 139, 139))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
+                .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(txtNum1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -105,13 +153,21 @@ public class frm_principal extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(txtNum2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(23, 23, 23)
+                .addGap(18, 18, 18)
+                .addComponent(rbSomar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(rbSubtrair)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(rbMultiplicar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(rbDividir)
+                .addGap(29, 29, 29)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtResultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(71, 71, 71)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addContainerGap(139, Short.MAX_VALUE))
+                .addContainerGap(80, Short.MAX_VALUE))
         );
 
         pack();
@@ -126,8 +182,54 @@ public class frm_principal extends javax.swing.JFrame {
     }//GEN-LAST:event_txtNum1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        String a = txtNum1.getText();
+        String b = txtNum2.getText();
+
+        int n1 = Integer.parseInt(a);
+        int n2 = Integer.parseInt(b);
+
+        if (rbSomar.isSelected()) {
+            int calculo = (n1 + n2);
+
+            String c = String.valueOf(calculo);
+
+            txtResultado.setText(c);
+        } else if (rbSubtrair.isSelected()) {
+            int calculo = (n1 - n2);
+
+            String c = String.valueOf(calculo);
+
+            txtResultado.setText(c);
+        } else if (rbMultiplicar.isSelected()) {
+            int calculo = (n1 * n2);
+
+            String c = String.valueOf(calculo);
+
+            txtResultado.setText(c);
+        }else if(rbDividir.isSelected()){
+            int calculo = (n1 / n2);
+
+            String c = String.valueOf(calculo);
+
+            txtResultado.setText(c);
+        }else{
+            JOptionPane.showMessageDialog(null, "Selecione uma operação!");
+        }
+
+
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void rbSomarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbSomarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rbSomarActionPerformed
+
+    private void rbDividirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbDividirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rbDividirActionPerformed
+
+    private void rbSubtrairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbSubtrairActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rbSubtrairActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,11 +267,16 @@ public class frm_principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JRadioButton rbDividir;
+    private javax.swing.JRadioButton rbMultiplicar;
+    private javax.swing.JRadioButton rbSomar;
+    private javax.swing.JRadioButton rbSubtrair;
     private javax.swing.JTextField txtNum1;
     private javax.swing.JTextField txtNum2;
     private javax.swing.JTextField txtResultado;
