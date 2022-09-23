6b0a2d23bd8d1897d87263bdda3317d95d7c9534
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-09-22 21:17:23 -0300

    Criação da interface de uma calculadora

--- src/interfacegrafica/frm_calculadora.java
+++ src/interfacegrafica/frm_calculadora.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author henrique.medeiros
  */
-public class frm_principal extends javax.swing.JFrame {
+public class frm_calculadora extends javax.swing.JFrame {
 
     /**
      * Creates new form frm_principal
      */
-    public frm_principal() {
+    public frm_calculadora() {
         initComponents();
     }
 
@@ -248,20 +248,21 @@ public class frm_principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frm_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frm_calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frm_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frm_calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frm_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frm_calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frm_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frm_calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frm_principal().setVisible(true);
+                new frm_calculadora().setVisible(true);
             }
         });
     }
